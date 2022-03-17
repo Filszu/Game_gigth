828799289c73b89e0323540a79b7edb91130a43d
Filszu <filipszumowski05@gmail.com>
2022-03-17 23:42:15 +0100

    every entity is now represent an object, every entity is in entity' array, and\n i every animationFrames - uptdate() function is for every object in arrays

--- game.js
+++ game.js
@@ -1,20 +1,31 @@
-
+//rodzeil do nowych plikow
 const board = document.querySelector('.background'); 
 const grassDiv = document.querySelector('.grass'); 
 
 
 const ch = board.clientHeight;
 const cw = board.clientWidth;
-const startY=-55;
 
+//pole rozgrywek
+const startMap=100;
+const EndtMap=ch-100;
+const startY=-55;//wysokosc trawy
+
+//tutja bedzie tablica na obiekty
 const playerDiv = document.querySelector(".player");
 
-let heroes = [];
+let heroes = [];//tablica nazw divow / chyba nie potrzebna xdd
 let enemies = [];
+let heroesOBJ = [] //tablica obiektow
 
+
+//Klasa Entity, bede po niej dziedziczyc hero i enemy ale nwm czy to potzebne
 class Entity{
 
     constructor(name, hp, attack, defense, lvl, speed) { 
+
+       
+        //mozna pozniej zmienic dodajc klay pochodne postaci, np peichur / mechanik
         this.name = name;
         this.hp = hp;
         this.attack = attack;
@@ -25,6 +36,27 @@ class Entity{
         this.isFighting = false;
         this.x=0;
         this.y=0;
+
+         //dodanie div reprezentujacego entity (do documentu)
+         var entityDiv = document.createElement('div');
+         entityDiv.className = "player";
+         entityDiv.id=`entity${heroes.length+1}`;
+
+         //-----------------chyba nie potzebne
+         heroes.push(entityDiv.id);//zapisanie po id postaci do tab
+         board.appendChild(entityDiv);
+        
+
+         //powiazanie div z obiektem
+         this.id=entityDiv.id;
+         this.docID = document.querySelector(`#${entityDiv.id}`);
+         //docID inaczej obiekt html reprezentujacy w graficzny sposob obiekt js
+
+
+         //od razu wywolanie przy tworzeniu
+         this.spawn();
+         this.move();
+
 
     }
 
@@ -43,12 +75,12 @@ class Entity{
 
     setX(move){
         this.x+=move;
-        playerDiv.style.left=this.x+"px";
+        this.docID.style.left=this.x+"px";
     }
     move(){
         // jesli nie walczy 
         this.setX(this.speed);
-        console.log(this.x);
+        // console.log(this.x);
     }
 
 }
@@ -57,15 +89,17 @@ class Hero extends Entity{
     constructor(name, hp, attack, defense, lvl, speed){
         super(name, hp, attack, defense, lvl, speed);
 
+        
+
     }
 
     spawn(){
-        this.x=100;
+        this.x=startMap;
         this.y=startY;
         // playerDiv.style.backgroundColor ="black";
-        playerDiv.style.bottom=this.y+"px";
-        playerDiv.style.left=this.x+"px";
-        playerDiv.style.animation=" spawn 1s";
+        this.docID.style.bottom=this.y+"px";
+        this.docID.style.left=this.x+"px";
+        this.docID.style.animation=" spawn 1s";
     }
 
     
@@ -74,9 +108,33 @@ class Hero extends Entity{
 }
 
 
-const player = new Hero("hero", 100, 10, 10, 1, 5);
-player.spawn();
-player.move();
+// const player = new Hero("hero", 100, 10, 10, 1, 5);
+
+
+
+//add new entity test
+heroesOBJ.push(new Hero("hero", 100, 10, 10, 1, 5));
+
+// heroesOBJ[0].spawn();
+// heroesOBJ[0].move();
+// player.spawn();
+// player.move();
+
+
+
+
+
+
+
+//update ruchow i wszytkich czynnowsci entity
+function update(){
+    for(let hero in heroes){
+        heroesOBJ[hero].move();
+    }
+}
+
+
+
 
 
 
@@ -85,7 +143,8 @@ player.move();
 //animation loop
 
 function animate(){
-    player.move();
+    
+    update();
     requestAnimationFrame(animate);
 }
 
